8026824f2205e4475f88e91309f81e31be96c157
Neryos <brhv0000@gmail.com>
2023-01-02 16:13:11 +0200

    topic list

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 import { useAuthContext } from './hooks/useAuthContext';
+import { Route } from 'react-router-dom'
 
 // Styles
 import './App.css'
